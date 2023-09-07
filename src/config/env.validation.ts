fdb9d03e81eb23c38902e874d96af7fcd9614fea
Nguyen Kiet <kiet.t.nguyen@taptap.com.vn>
2023-09-07 16:20:13 +0700

    remove env

--- src/config/env.validation.ts
+++ src/config/env.validation.ts
@@ -1,18 +1,18 @@
 import * as Joi from 'joi';
 
 export const schema = {
-  APP_ENV: Joi.string().default('local'),
-  APP_NAME: Joi.string().default('game-gm-be'),
-  PORT: Joi.number().default(3010),
+  // APP_ENV: Joi.string().default('local'),
+  // APP_NAME: Joi.string().default('game-gm-be'),
+  // PORT: Joi.number().default(3010),
 
   // rate limit
   THROTTLE_TTL: Joi.number().default(60),
   THROTTLE_LIMIT: Joi.number().default(120),
 
-  // redis
-  REDIS_HOST: Joi.string().required(),
-  REDIS_PORT: Joi.number().required(),
-  REDIS_PREFIX: Joi.string().required(),
+  // // redis
+  // REDIS_HOST: Joi.string().required(),
+  // REDIS_PORT: Joi.number().required(),
+  // REDIS_PREFIX: Joi.string().required(),
 
   // game api
   // MY_API: Joi.string().required(),
